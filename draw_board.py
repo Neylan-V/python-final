8494ba6d57832411dacabd9c02b8a1ee471afd52
nvisnius <117500512+Neylan-V@users.noreply.github.com>
2022-11-10 02:41:38 +0000

    nothing much! added some misc files for future use

---
+++ draw_board.py
@@ -0,0 +1,18 @@
+DEBUG = True
+
+def empty_board():
+  print("empty board")
+
+
+
+
+
+
+
+
+
+
+
+
+if DEBUG:
+  print("hey it accessed draw_board! crazy.")
---
+++ headspace.py
@@ -0,0 +1,35 @@
+########## WHAT AM I DOING??????
+# what functions?
+# what main body?
+# ...classes?
+  # maybe the whole GAME is a class?
+  # ok maybe not, based on the wheel of python reading, but the players could be classes?
+    # maybe the board IS a class? what woud it do? would the updating of it go in a class or in the main program?
+    # maybe update would be something like... board.update(current_board, start_pocket, num_tiles) 
+
+
+
+
+
+
+##### MISC NOTES
+# game end could be a checker that looks for one side of the board to be empty
+
+
+
+### imports?
+
+### classes
+# define human_player class
+# define computer_player class
+
+### functions?
+
+### main
+# welcomes players, describes rules? maybe gives options to describe rules if needed
+# draws board
+# asks for num human players
+  # creates instances of for those player(s)
+  # if only one, creates instance for computer player
+#AND THEN?
+  
